add3807879c427a6244b2b800ca2563f2dc80d23
CanIALugRoamOn <romanogianluca@outlook.com>
2018-06-12 18:10:22 +0200

    first commit

---
+++ VRPT/Assets/Scripts/FeedbackManager.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ConnectorHubUW;
+
+public class FeedbackManager : MonoBehaviour {
+
+
+
+#if NETFX_CORE
+    public FeedbackHub myFeedbackHub;
+#endif
+    public Sprite[] IconCollection;
+    public GameObject PTIconHolder;
+    //public int listeningTCPPort;
+    private UnityEngine.UI.Image IconImage;
+    private UnityEngine.UI.Text IconText;
+    // Only for testing
+    public string[] FeedbackSequence;
+    private int iterator = 1;
+    private bool coroutine = true;
+
+    private string feedback = "Good";
+    // Use this for initialization
+    void Start() {
+
+#if NETFX_CORE
+
+        myFeedbackHub = new FeedbackHub();
+        myFeedbackHub.init(listeningTCPPort, listeningUDPPort);
+        myFeedbackHub.feedbackReceivedEvent += MyFeedbackHub_feedbackReceivedEvent();
+#endif
+        // Get Image component of the Icon
+        IconImage = PTIconHolder.GetComponent<UnityEngine.UI.Image>();
+        // make sure to be transparent
+        IconImage.color = new Color32(255, 255, 255, 0);
+
+        // Get Text Component of the Icon
+        IconText = PTIconHolder.GetComponentInChildren<UnityEngine.UI.Text>();
+
+        feedback = "";
+        feedback = FeedbackSequence[0].ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // TO DO if feedback == "good" then ...     
+        if (coroutine){
+            IconImage.color = new Color32(255, 255, 255, 255);
+
+            if (feedback.Contains("Good"))
+            {
+                IconImage.sprite = IconCollection[0];
+                IconText.text = "Good !";
+            }
+            else if (feedback.Contains("Reset Posture"))
+            {
+                IconImage.sprite = IconCollection[1];
+                IconText.text = "Reset Posture";
+            }
+            else if (feedback.Contains("Stand Still"))
+            {
+                IconImage.sprite = IconCollection[1];
+                IconText.text = "Stand Still";
+            }
+            else if (feedback.Contains("Move Hands"))
+            {
+                IconImage.sprite = IconCollection[1];
+                IconText.text = "Move Hands";
+            }
+            else if (feedback.Contains("Smile"))
+            {
+                IconImage.sprite = IconCollection[2];
+                IconText.text = "Smile";
+            }
+            else if (feedback.Contains("Speak Louder"))
+            {
+                IconImage.sprite = IconCollection[3];
+                IconText.text = "Speak Louder";
+            }
+            else if (feedback.Contains("Speak Softer"))
+            {
+                IconImage.sprite = IconCollection[4];
+                IconText.text = "Speak Softer";
+            }
+            else if (feedback.Contains("Stop Speaking"))
+            {
+                IconImage.sprite = IconCollection[6];
+                IconText.text = "Stop Speaking";
+            }
+            else if (feedback.Contains("Stop Hmmmm"))
+            {
+                IconImage.sprite = IconCollection[6];
+                IconText.text = "Stop Hmm";
+            }
+            else if (feedback.Contains("Start Speaking"))
+            {
+                IconImage.sprite = IconCollection[5];
+                IconText.text = "Start Speaking";
+            }
+            else
+            {
+                // wait for some seconds before resetting the icon
+                IconImage.sprite = null;
+                IconImage.color = new Color32(255, 255, 255, 0);
+                IconText.text = "";
+            }
+            feedback = FeedbackSequence[iterator].ToString();
+            if (iterator >= FeedbackSequence.Length - 1)
+            {
+                iterator = 0;
+            }
+            else
+            {
+                iterator++;
+            }
+        }
+        else
+        {   
+            //StartCoroutine(WaitBeforeReset());
+        }
+        //coroutine = false;
+        
+        
+
+    }
+
+    public IEnumerator WaitBeforeReset()
+    {
+        yield return new WaitForSeconds(.5f);
+        coroutine = true;
+        
+    }
+
+#if NETFX_CORE
+    public myFeedbackHub myFeedbackHub_feedbackReceivedEvent(object sender,string feedback)
+    {
+        this.feedback = feedback;
+    }
+#endif
+}
---
+++ VRPT/Assets/Scripts/RecordingManager.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecordingManager : MonoBehaviour {
+
+    public GameObject ButtonPlay;
+    public GameObject ButtonPause;
+    public GameObject ButtonStop;
+
+    private InputField Input; 
+    public GameObject Menu;
+    public GameObject Init;
+    // init ip adress of machine in network
+    private string IPAdress;
+    private string Port = "12345";
+
+
+
+	// Use this for initialization
+	void Start () {
+        IPAdress = "";
+
+        Input = GameObject.Find("InputField").GetComponent<InputField>();
+
+        Button StartApp = GameObject.Find("StartApp").GetComponent<Button>();
+        StartApp.onClick.AddListener(() => TaskOnClick(StartApp));
+
+        Button Play = ButtonPlay.GetComponent<Button>();
+        Play.onClick.AddListener(() => TaskOnClick(Play));
+
+        Button Pause = ButtonPause.GetComponent<Button>();
+        Pause.onClick.AddListener(() => TaskOnClick(Pause));
+
+        Button Stop = ButtonStop.GetComponent<Button>();
+        Stop.onClick.AddListener(() => TaskOnClick(Stop));
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // get the IP Adress from the input
+        IPAdress = Input.GetComponent<InputField>().text;
+    }
+
+   public void TaskOnClick(Button Btn)
+    {
+        switch (Btn.name)
+        {
+            case "StartApp":
+                if (IPAdress != "")
+                {
+                    // change panels
+                    Menu.SetActive(true);
+                    Init.SetActive(false);
+                    sendMessage("<START APPLICATIONS>", IPAdress, Port);               
+                }
+                else
+                {
+                    Input.GetComponent<InputField>().placeholder.GetComponent<Text>().text = "IP Adress?";
+                    
+                }
+
+                break;
+            case "Play":
+                sendMessage("<START RECORDING>",IPAdress,Port);
+                break;
+            case "Pause":
+                sendMessage("<STOP RECORDING>", IPAdress, Port);
+                break;
+            case "Stop":
+                sendMessage("<STOP RECORDING>", IPAdress, Port);
+                break;
+            case "Finish":
+                sendMessage("<FINISCH>", IPAdress, Port);
+                break;
+        }
+    }
+
+    // the messages can be: "<START APPLICATIONS>" "<START RECORDING>" "<STOP RECORDING>" "<FINISH>"
+    private void sendMessage(string message, string IPAddress, string port)
+    {
+#if NETFX_CORE
+        try
+        {
+            // Create the StreamSocket 
+            using (var streamSocket = new Windows.Networking.Sockets.StreamSocket())
+            {
+                // The server hostname that we will be establishing a connection to. In this example, the server and client are in the same process.
+                var hostName = new Windows.Networking.HostName(IPAddress);
+
+                await streamSocket.ConnectAsync(hostName, port);
+
+                using (Stream outputStream = streamSocket.OutputStream.AsStreamForWrite())
+                {
+                    using (var streamWriter = new StreamWriter(outputStream))
+                    {
+                        await streamWriter.WriteLineAsync(message);
+                        await streamWriter.FlushAsync();
+                    }
+                }
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Windows.Networking.Sockets.SocketErrorStatus webErrorStatus = Windows.Networking.Sockets.SocketError.GetStatus(ex.GetBaseException().HResult);
+
+        }
+#endif
+    }
+
+}
